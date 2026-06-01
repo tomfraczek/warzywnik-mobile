4ad01c28872f9bb3a9f4f343e73976b0bd1e312e
Tomasz Fraczek <t.fraczek@gmi.pl>
2026-06-01 10:54:36 +0200

    feat: Register Polish translation for date picker in AuthFlowLoader

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,6 @@
 import { AuthFlowLoader } from "@/src/components/AuthFlowLoader";
+import { pl, registerTranslation } from "react-native-paper-dates";
+registerTranslation("pl", pl);
 
 export default function Index() {
   return <AuthFlowLoader />;
